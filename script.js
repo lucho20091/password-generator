c9f86704bcdd052c86ce1c3b4d8ae7b07b1f14bb
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2024-06-16 15:50:49 -0500

    commit

--- script.js
+++ script.js
@@ -5,67 +5,39 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
 "@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",",
 "|",":",";","<",">",".","?","/"];
 
+const password1Elem = document.getElementById("password1");
+const password2Elem = document.getElementById("password2");
+const generatedElem = document.getElementById("generated");
 
-const btnEl = document.querySelector("#btn-generate")
-let password1 = []
-let password2 = []
-const passwordEl1 = document.querySelector("#password1")
-const passwordEl2 = document.querySelector("#password2")
-const displayall = document.querySelector(".displayall")
-const copiedMessageEl = document.querySelector("#copiedMessage")
-const selectEl = document.querySelector("#select-value")
-
-
-btnEl.addEventListener("click", () => {
-    password1 = []
-    password2 = []
-    passwordEl1.classList.add("password")
-    passwordEl2.classList.add("password")
-    for (let i = 0; i < selectEl.value; i++){
-        let random = Math.floor(Math.random() * 95)
-        let random2 = Math.floor(Math.random() * 95)
-        password1.push(characters[random])
-        password2.push(characters[random2])
-    }
-    display()
-    all()
-    console.log(password1)
-    console.log(password2)
-})
-
-function display() {
-        passwordEl1.innerHTML = `<i class="fa-regular fa-copy"></i>` + " " + password1.join("")
-        passwordEl2.innerHTML = `<i class="fa-regular fa-copy"></i>` + " " + password2.join("")
+function generate(){
+    generatedElem.innerHTML = renderingHtml(createRandom(), createRandom());
+    console.log(generatedElem.innerHTML)
 }
 
-function all(){
-    displayall.textContent = characters
+function createRandom(){
+    let passwordRandom = ''
+    for (let i = 0; i < 10; i++){
+        passwordRandom += characters[random()]
+    }
+    return passwordRandom
 }
 
-function showCopiedMessage() {
-    copiedMessageEl.textContent = "Copied Successfully"
-    setTimeout(() => {
-        copiedMessageEl.textContent = ""
-    }, 2000)
+function random(){
+    return (Math.floor(Math.random() * characters.length));
 }
 
-passwordEl1.addEventListener("click", async () => {
-    try {
-        copiedMessageEl.textContent = ""
-        await navigator.clipboard.writeText(passwordEl1.textContent)
-        showCopiedMessage()
-    } catch (e) {
-        console.log(e)
-    }
-})
+function copyText(event){
+    const { innerText } = event.target
+    navigator.clipboard.writeText(innerText)
+    alert("copied the text " + innerText)
+}
 
-passwordEl2.addEventListener("click", async () => {
-    try {
-        copiedMessageEl.textContent = ""
-        await navigator.clipboard.writeText(passwordEl2.textContent)
-        showCopiedMessage()
-    } catch (e) {
-        console.log(e)
+function renderingHtml(...string){
+    console.log(string)
+    let html = ''
+    for (let i = 0; i < 2; i++){
+        html += `<p class="password" onclick="copyText(event)">${string[i]}</p>`
     }
-})
-
+    console.log(html)
+    return html
+}
